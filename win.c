6af4fc4e04291dad6e993b17ea80321d9227aa45
devloves <65783463+devloves@users.noreply.github.com>
2021-04-23 21:49:59 +0530

    Add files via upload

---
+++ win.c
@@ -0,0 +1,7 @@
+#include <windows.h>
+
+int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
+{
+    return MessageBox(NULL, "Testing", " ROCK PAPER SCISSORS By Pranav Ginoya ", 0);
+}
+ 
